c063d17679d49bc3a44f398f7dae42b98b9fa431
Noravee <150005712+Noravee@users.noreply.github.com>
2024-02-05 14:50:36 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning to predict superconducting transition temperature',
+    author='Noravee Kanchanavatee',
+    license='MIT',
+)
